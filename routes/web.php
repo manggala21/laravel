8de6db0293f5c42734964e1458d538f9645f4563
manggala21 <68697995+manggala21@users.noreply.github.com>
2023-09-01 20:41:19 +0700

    Update Step 4

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
